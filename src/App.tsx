b7bdc76f26344e24ce4d24990773db5513750b8f
mkhan305 <mohammadkhan@college.harvard.edu>
2024-02-25 23:06:44 -0500

    comp 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,97 @@
-function App() {
+import React, { useState } from 'react';
+import Modal from './components/Modal'; 
+
+
+function App() : JSX.Element {
+
+  const [input, setInput] = useState<string>('');
   // TODO: fix the state of this `todos`
-  const todos = ['Do the dishes.', 'Finish this project.']
+  const [todos, setTodos] = useState<string []>(['Do the dishes.', 'Finish this project.']); 
+  
+  function Task( { name, num } : { name : string, num: number}) { 
+    const [hovered, setHovered] = useState<boolean>(false);
+    
+    function handleDeleteTask() : void {
+      todos.splice(num, 1); 
+      setTodos([...todos])
+    }
+    return ( 
+      <li onClick={handleDeleteTask} 
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}>
+        <label> 
+          <input style={{marginRight: '10px', backgroundColor: 'black'}} type="checkbox" checked={hovered} /> 
+          {hovered ? <s>{name}</s> : name}
+        </label> 
+      </li> 
+    )
+  }
+
+  // handles form submit 
+  function formSubmit(event: React.FormEvent<HTMLFormElement>) : void { 
+    event.preventDefault(); 
+
+    // ensures input is not empty 
+    if (input.trim() != '') { 
+      setTodos([...todos, input])
+      setInput('');
+    }
+  } 
+
+  function formChange(event : React.ChangeEvent<HTMLInputElement>) : void { 
+    event.preventDefault(); 
+    setInput(event.currentTarget.value); 
+  }
+
+
   // TODO: make components needed for TODO
   return (
-    <>
-      <ul>
-        {
-          // TODO: use `map` to render tasks from `todos`
-          // remember about `keys` prop!
-        }
-      </ul>
+    <> 
+      <div style={{ height: '100vh', display: 'flex', alignItems: 'center' }}> 
+        <div style={{ display: 'flex', 
+                      border: '2px solid black', 
+                      flexDirection: 'column', 
+                      width: '40%',
+                      minHeight: '50%', 
+                      margin: 'auto', 
+                      borderRadius: '20px', 
+                      padding: '3rem', 
+                      justifyContent: 'space-between', 
+                      alignItems: 'flex-start', 
+                      background: 'white',
+                    }}> 
+          <div style={{ width: '100%' }}> 
+            <h1>to do</h1>
+            <hr />
+          </div>  
+          <ul style={{ listStyleType: 'none', flexGrow: 1 }}>
+            {
+              todos.map((task, index) => { 
+                return <Task name={task} num={index} key={index}/> 
+              })
+            }
+          </ul>
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', width: '100%', gap: '40px'}}> 
+            <form
+              style={{
+               flexGrow: 1, display: 'flex', gap: '20px'
+              }}
+              onSubmit={formSubmit}
+            >
+              <input 
+                value={input}
+                onChange={formChange}
+                placeholder="task"
+                style={{ flexGrow: 1, fontSize: '20px'}}
+              />
+              <button type="submit" style={{ background: 'black', color:'white' }}>create task</button>
+            </form>
+            <Modal />
+          </div> 
 
-      <form
-        style={{
-          marginTop: '10px',
-        }}
-      >
-        {
-          // TODO: lift input text into a state so you can
-          // handle it and add to `todos` array
-        }
-        <input />
-        <button>Create Task</button>
-      </form>
 
-      <Modal />
+        </div>
+      </div> 
     </>
   )
 }
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,28 +1,49 @@
 import { createPortal } from 'react-dom'
+import { useState } from 'react';
 
 const modalRoot = document.getElementById('modal')
 
 export default function Modal() {
   // access modal root element with
   // this is where the modal should be rendered and destroyed
+
+  const [open, setOpen] = useState<boolean>(false); 
+
   return (
     <>
-      {
-        // TODO: handle button click to change state
-      }
-      <button>Click Me!</button>
+      <button type="button"
+        onClick={() => setOpen(true)} 
+        style={{ background: 'black', color:'white', height: 'auto' }}>
+          click me!
+      </button>
 
-      {
-        // TODO: conditionally show this based on open state
-      }
-      {createPortal(
-        <div>
-          {
-            // TODO: style with the specifications given in the README
-          }
-        </div>,
-        modalRoot as Element,
-      )}
+      { open ? 
+        createPortal(
+          <div style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            height: '20rem', 
+            width: '20rem', 
+            transform: 'translate(-50%, -50%)', 
+            backgroundColor: 'white',
+            display: 'flex',
+            flexDirection: 'column', 
+            justifyContent: 'center',
+            alignItems: 'center',
+            border: '2px solid black', 
+            borderRadius: '20px'
+          }}>
+            <p>modal lmao</p> 
+            <button onClick={() => setOpen(false)}>exit lmao</button> 
+
+            {
+              // TODO: style with the specifications given in the README
+            }
+          </div>,
+          modalRoot as Element,
+        ) 
+        : null } 
     </>
   )
 }
